2b3a3fb49c7aafbf78ae3943e49fb52b4943d9e8
Rév O'Conner <88772846+revoconner@users.noreply.github.com>
2021-10-03 12:58:36 +0530

    Delete Set_Bone_Data.py

--- Set_Bone_Data.py
+++
@@ -1,6 +0,0 @@
-from maya import cmds
-import maya.api.OpenMaya as om
-
-selected = cmds.select(bone)
-#cmds.xform(selected,ws=1,t=(offsetX, offsetY, offsetZ))
-cmds.move( offsetX, offsetY, offsetZ, absolute=True, ws=True, pcp=True )
